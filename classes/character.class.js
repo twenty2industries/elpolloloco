4450daca9cb4afa5ec820a5efd89335383945538
twenty2industries <burak.bostanli.1@web.de>
2025-05-15 14:22:48 +0200

    FEAT: character now running left & right using static variables

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,35 +1,43 @@
 class Character extends MovableObject {
   height = 200;
-  y = 230
+  y = 230;
+  speed = 3;
   IMAGES_WALKING = [
-      'img/2_character_pepe/2_walk/W-21.png',
-      'img/2_character_pepe/2_walk/W-22.png',
-      'img/2_character_pepe/2_walk/W-23.png',
-      'img/2_character_pepe/2_walk/W-24.png',
-      'img/2_character_pepe/2_walk/W-25.png',
-      'img/2_character_pepe/2_walk/W-26.png'
-    ];
+    "img/2_character_pepe/2_walk/W-21.png",
+    "img/2_character_pepe/2_walk/W-22.png",
+    "img/2_character_pepe/2_walk/W-23.png",
+    "img/2_character_pepe/2_walk/W-24.png",
+    "img/2_character_pepe/2_walk/W-25.png",
+    "img/2_character_pepe/2_walk/W-26.png",
+  ];
 
-  constructor(){ //wird immer als aller erstes ausgeführt sobald new Character erstellt wird
-    super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+  constructor() {
+    //wird immer als aller erstes ausgeführt sobald new Character erstellt wird
+    super().loadImage("img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
     this.animate();
   }
 
-  animate(){
+  animate() {
     setInterval(() => {
-      
       if (Keyboard.RIGHT) {
-           let i = this.currentImage % this.IMAGES_WALKING.length; 
-    // let i = 0 % 6 
-    let path = this.IMAGES_WALKING[i];
-    this.img = this.imageCache[path];
-    this.currentImage++; 
+        this.x += this.speed;
+      }
+
+      if (Keyboard.LEFT) {
+        this.x -= this.speed;
+      }
+    }, 1000 / 60);
+
+    setInterval(() => {
+      if (Keyboard.RIGHT || Keyboard.LEFT) {
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
       }
     }, 100);
   }
 
-  jump() {
-    
-  }
+  jump() {}
 }
